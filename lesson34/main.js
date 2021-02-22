78e4d4ea39d143f4a0933ad4c7a3719855a51620
Dana123098 <74066114+Dana123098@users.noreply.github.com>
2021-02-22 19:49:56 +0200

    Add files via upload

---
+++ lesson34/main.js
@@ -0,0 +1,101 @@
+//let x = prompt("сколько тебе лет ? ")
+
+//if(x < 7){
+  //  alert("ты малыш")
+//}
+//else if(x < 18){
+  //  alert("ты подросток")
+//}
+//else if(x < 60){
+ //   alert("вы взрослый")
+//}
+//else{
+//    alert("вы старик" )
+//}
+
+
+
+///let x = prompt("ср числа x ")
+
+//let a = prompt("ср числа a")
+
+
+
+//if(x > a){
+//alert("x > a")
+//}
+//else if(x < a){
+//alert("x < a")
+ //}
+
+ //if(x == a){
+    // alert("x = a")
+    // }
+ //else if(x != a){
+    // alert("x 1= a")
+     // }
+
+
+      //let y = prompt("ср числа  y")
+     //let t = prompt("ср числа t")
+
+
+      //if(y > t){
+       // alert("y > t")
+        //}
+       // else if(y < t){
+       // alert("y < t")
+       //  }
+        ///
+        /// if(y == t){
+           //  alert("y = t")
+            // }
+        // else if(x != a){
+            // alert("y 1= t")
+            //  }
+
+
+
+
+            //let a = prompt("это равнобед тр")
+       // if(a == "да"){
+            //alert("2 стороны ровны")
+        //}
+        //else {
+          //  alert("тр разност.")
+        //}
+        
+
+
+
+       // let a = prompt("вы кто?")
+       // if(a == "админ"){
+          //  alert("здравствуйте")
+          //  let x = prompt("вед пароль")
+            //console.log(x)
+        //}
+        //else if(a == "деректор"){
+         //   alert("добро пожаловать")
+        //}
+        //else if(a == "пользователь"){
+        //    alert("рег")
+        //}
+       // else {
+         //   alert("такого п не найдено")
+        //}
+
+
+let a = prompt("решил или нет")
+
+    if (a == "да"){
+        alert("иди домой")
+    }
+
+   else if (a == "нет"){
+        alert("оставайся")
+        let w = prompt("до скольки?")
+        alert("до " + w)
+    }
+    else if (a == "за 5 мин"){
+        alert("всем еда")
+    }
