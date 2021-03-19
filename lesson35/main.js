0ce29a58c19911bd187ccb636b9ffa3887375f21
Dana123098 <74066114+Dana123098@users.noreply.github.com>
2021-03-19 17:57:22 +0200

    Add files via upload

---
+++ lesson35/main.js
@@ -0,0 +1,110 @@
+//let buool = 1 || 0
+//alert(buool)
+
+
+//let bool = 1 || 1
+//alert(bool)
+
+//let bol = 0 || 1
+//alert(bol)
+
+
+//let by = 0 || 0
+//alert(by)
+
+
+
+
+
+
+//let x = prompt("вед время ")
+//if(x < 10 || x > 18){
+  //  alert("не работакт")
+//}
+//else{
+  //  alert("работает")
+//}
+
+
+
+//let buool = 1 && 0
+//alert(buool)
+
+//let buol = 0 && 1
+//alert(buol)
+
+//let bol = 1 && 1
+//alert(bol)
+
+//let bu = 0 && 0
+//alert(bu)
+
+
+//alert(1 && 0)
+
+//alert(1 && 5)
+//alert(null && 5)
+//alert(1 && "string")
+//alert(1 && 2 && null && "gfslkngls")
+//alert(a && b || c && d)
+
+
+//let x = !1
+//alert(x)alert(alert(1) ||2|| alert(3))
+//let y = !0
+//alert(y)
+
+
+
+//alert(!"dtyedhed")
+//alert(!!null)
+
+
+//alert(alert(1) ||2|| alert(3))
+//alert(alert(1) &&2&& alert(3))
+
+
+//alert(null || 2 && 3 || 4)
+
+//let x = prompt("кто?")
+//if(x == "оператор"){
+//     alert("ваш д не найден")
+// }
+// if(x == "другое"){
+//     alert("я вас не знаю")
+// }
+// if(x == "админ"){
+//     let z = prompt("пароль")
+//     console.log(z)
+    
+// }
+// else if(z == "pop"){
+//   alert("приветствую вас  " + z)
+// }
+// else if(z != "pop"){
+//     alert("пароль не верный")
+// }
+
+
+let x = prompt("кто ты?");
+
+if (x == "админ"){
+
+  let p = prompt("пароль?");
+
+  if (p == "111"){
+    alert("здравствуйте");
+  }
+  
+  else{
+    alert("неверный пароль");
+  }
+}
+
+
+else if (x == ""){
+  alert("ваш дей не зарегестрированны здесь");
+}
+else{
+  alert("я вас не знаю")
+}
