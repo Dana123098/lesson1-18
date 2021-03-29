ec277cc157d605191e2344cea8facd1f0e35c9ee
Dana123098 <74066114+Dana123098@users.noreply.github.com>
2021-03-29 18:55:49 +0300

    Add files via upload

---
+++ lesson44/main.js
@@ -0,0 +1,179 @@
+//let a = prompt()
+//switch(a){
+   // case a > 0;
+   // alert()
+    //break;
+
+    //case a > 0;
+    //alert()
+   // break;
+//default:
+   // alert()
+//}
+
+
+//let a = prompt("вед т")
+//switch(a){
+   // case "ru"
+    //alert("текст р")
+   /// case "n"
+    //alert("текст р")
+//}
+
+//1
+//let a = prompt("вед ч")
+//switch(a == 0){
+   // case true:
+   // alert("верно")
+   // break;
+
+   // default:
+       // alert("не верно")
+//}
+
+
+//2
+//let a = prompt("вед ч")
+//switch(a > 0){
+   /// case true:
+   // alert("верно")
+    //break;
+
+  // default:
+       // alert("не верно")
+//}
+
+//3
+//let a = prompt("вед ч")
+//switch(a < 0){
+     //case true:
+    //alert("верно")
+     //break;
+ 
+   // default:
+        // alert("не верно")
+ //}
+
+ //4
+ //let a = prompt("вед ч")
+//switch(a >= 0){
+     //case true:
+    //alert("верно")
+    // break;
+ 
+    //default:
+       //  alert("не верно")
+ //}
+ //5
+ //let a = prompt("вед ч")
+//switch(a <= 0){
+    // case true:
+   // alert("верно")
+    // break;
+ 
+    //default:
+        // alert("не верно")
+//}
+
+//6
+//let a = prompt("вед ч")
+//switch(a != 0){
+     //case true:
+   // alert("верно")
+    // break;
+ 
+    //default:
+       //  alert("не верно")
+ //}
+ //7
+ //let a = prompt("вед ч")
+//switch(a == "test"){
+    // case true:
+    //alert("верно")
+    // break;
+ 
+    //default:
+       //  alert("не верно")
+ //}
+
+ //8
+ //let a = prompt("вед ч")
+//switch(a == '"1"' || a == 1){
+     //case true:
+    //alert("верно")
+    // break;
+ 
+    //default:
+         //alert("не верно")
+ //}
+
+
+ //1
+ //let a = prompt("вед т")
+ //if(a == 0){
+     //alert("верно")
+ //}
+ //else{
+    // alert("ен верно")
+ //}
+
+ //2
+ //let a = prompt("вед т")
+ //if(a > 0){
+    // alert("верно")
+ ///}
+ //else{
+     //alert("ен верно")
+ //}
+
+ //3
+ //let a = prompt("вед т")
+ //if(a < 0){
+    // alert("верно")
+ //}
+ //else{
+     //alert("ен верно")
+ //}
+
+ //4
+ //let a = prompt("вед т")
+ //if(a >= 0){
+    // alert("верно")
+ //}
+ //else{
+     //alert("ен верно")
+ //}
+
+ //5
+ //let a = prompt("вед т")
+ //if(a <= 0){
+    // alert("верно")
+ //}
+ //else{
+     //alert("ен верно")
+ //}
+ //let a = prompt("вед т")
+ //if(a != 0){
+    // alert("верно")
+ }
+ //else{
+     //alert("ен верно")
+ }
+ //7
+
+ //let a = prompt("вед т")
+ //if(a == "test"){
+    // alert("верно")
+ //}
+ //else{
+     //alert("ен верно")
+ //}
+
+ //8
+ let a = prompt("вед т")
+ if(a == "1" || a == 1){
+     alert("верно")
+ }
+ else{
+     alert("ен верно")
+ }
