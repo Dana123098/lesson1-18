3d0aa3e2698108f0c0311639eb2341668b9020c7
Dana123098 <74066114+Dana123098@users.noreply.github.com>
2021-04-16 18:08:38 +0300

    Add files via upload

---
+++ lesson49/main.js
@@ -0,0 +1,73 @@
+// function showMessege(){
+//   alert("привет саша")
+// }
+// showMessege();
+
+// function showMessege(){
+//   let a = ("ПРИВЕТ")
+// alert(a)
+// }
+// showMessege();
+
+
+// function showMessege(){
+//   let a = prompt("ПРИВЕТ")
+// alert(a)
+// }
+// showMessege();
+
+
+
+// let myName = prompt("как тебя зовут")
+// function showMessege(){
+//   let a = "ПРИВЕТ" + " " + myName
+// alert(a)
+// }
+// showMessege();
+
+// function showMessege(from, text){
+//      alert(from + " " + "получит " + " " + text)
+// }
+// showMessege("dana" , "diplom");
+
+
+// function showMessege(a, b){
+//   alert(a + " " + "кот ел" + " " + b)
+// }
+
+// let a = prompt("как он ел ")
+// let b = prompt("где")
+// showMessege(a, b);
+
+// let a = Number(prompt("a="))
+// let b = Number(prompt("b="))
+// function sum(a, b){
+//   return a + b
+
+
+
+//Дз
+
+
+
+
+// }
+// let c = sum(a, b)
+// alert(c)
+
+// let a = Number(prompt("a="))
+// let b = Number(prompt("b="))
+// function sum(a, b){
+//   return a * b
+
+// }
+// let c = sum(a, b)
+// alert(c)
+
+
+function showMessege(a, b){
+  alert(a + " " + "мальчик ходил" + " " + b)
+}
+let a = prompt("как ")
+let b = prompt("где")
+showMessege(a, b);
